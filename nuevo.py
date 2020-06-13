c70b6933a64ae2f9c41abd32eb67325d0a594516
Isolde <kouga85@gmail.com>
2020-06-12 18:14:00 -0700

    primeros pasos

--- nuevo.py
+++ nuevo.py
@@ -1,1 +1,12 @@
-ssss sss ddd
+#cadenas
+print('texto en dos \nlineas')
+print('texto en dos \tespacios')
+print('texto en dos \respacios')
+print("""un ejemplo 
+para ver que pasa""")
+
+Alumno = ("Alejandra")
+print("el quinto caracter es", Alumno[3])
+
+
+
