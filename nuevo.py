747bba2ec0fa7f94124bd5367f3421e34a90dd0e
Isolde <kouga85@gmail.com>
2020-06-15 18:09:40 -0700

    repasando lo basico

--- nuevo.py
+++ nuevo.py
@@ -1,12 +1,31 @@
 #cadenas
 print('texto en dos \nlineas')
 print('texto en dos \tespacios')
-print('texto en dos \respacios')
+print(r'C:\Users\Hiroki')
 print("""un ejemplo 
 para ver que pasa""")
 
 Alumno = ("Alejandra")
-print("el quinto caracter es", Alumno[3])
 
+print("el caracter es", Alumno[0:3])
+print("La Cantidad de caracteres es:", len(Alumno))
 
+####listas con asignacion######
+listas = [0,1,2,3,4,5,5,6]
+listas[6] = 6
+listas[7] = 7
+listas.append(8) # es para agregar un dato a la lista
+print(listas)
 
+frutas = ['manzana', 'naranja', 'pera']
+frutas.append('zanahoria')
+print(frutas)
+
+###Dato por teclado
+
+Dato = input()
+
+print("tu nombre es:", Dato, "\nHola", Dato)
+
+Data = input()
+print("la suma de un numero:", int(Data)+5)
